b3de85550f23caf90482b20707ff862ba15638d2
butyukunn82 <butyukunn82@gmail.com>
2026-09-05 13:57:37 +0900

    Refresh PWA cache for terminology reflex update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='st-reflex-2026-v6';
+const CACHE='st-reflex-2026-v7';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./questions.js','./questions-extra.js','./glossary-meta.js','./glossary-ui.js','./thinking.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
